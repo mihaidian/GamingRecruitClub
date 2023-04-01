cb57bf264dc6a929dae2e62c26425b5884458f2a
mihai <mihaidian979@gmail.com>
2023-04-01 13:37:47 +0300

    Update

--- GameRecruitment/Models/CompanyInfoModel.cs
+++ GameRecruitment/Models/CompanyInfoModel.cs
@@ -5,12 +5,14 @@ namespace GameRecruitment.Models
     public class CompanyInfoModel
     {
         [Key]
-       
         public Guid CompanyId { get; set; }
         [Display(Name = "Company Name")]
+        [Required]
         public string CompanyName { get; set; }
+        [Required]
         [Display(Name = "Recruiting?")]
         public bool Recruiting { get; set; }
+        [Required]
         [Display(Name = "Member Since")]
         public DateTime MemberSince { get; set; }
     }
